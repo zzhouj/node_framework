b19050ba5c701b5047681f66d52955d1b3dd70e5
justin <zzhouj@yahoo.com>
2016-07-05 12:48:46 +0800

    bugfix: 源站配置了有set-cookie，所以节点不会缓存

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'villa', cookie: { maxAge: 15 * 60 * 1000 }}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'villa', cookie: { maxAge: 15 * 60 * 1000 }}));
 
 app.use('/', routes);
 
